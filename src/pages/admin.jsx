419233ac23cdee039342e721d77a12195bff7efc
AmashaKasunthi <akasunthi@gmail.com>
2026-01-09 19:38:22 +0530

    Admin_section📌

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,22 +1,26 @@
 import { Link, Route, Routes } from "react-router-dom";
 import { FaRegListAlt } from "react-icons/fa";
-import { FaCommentAlt } from "react-icons/fa";
+import { MdOutlineInventory2 } from "react-icons/md";
+import { LuUsersRound } from "react-icons/lu";
+import AdminProductsPage from "./admin/adminProductsPage";
+import AdminAddProductPage from "./admin/adminAddProductPage";
 
 export default function AdminPage(){
     return(
         <div className="w-full h-full border-4  flex bg-accent">
-            <div className="w-75 h-full flex flex-col bg-accent">
-                
-                <Link to="/admin/"> <FaRegListAlt /> Orders</Link>
-                <Link to="/admin/products">Products</Link>
-                <Link to="/admin/users">Users</Link>         
-                <Link to="/admin/reviews"> <FaCommentAlt />Reviews</Link>       
+            <div className="w-75 h-full  flex flex-col bg-accent text-white text-xl">
+                <h1 className="text-3xl font-bold p-5 border-b-4 border-white">Admin Panel</h1>
+                <Link className="flex w-full p-2.5 gap-3 items-center hover:bg-white hover:text-accent" to="/admin/"><FaRegListAlt /> Orders</Link>
+                <Link className="flex w-full p-2.5 gap-3 items-center hover:bg-white hover:text-accent" to="/admin/products"><MdOutlineInventory2 />Products</Link>
+                <Link className="flex w-full p-2.5 gap-3 items-center hover:bg-white hover:text-accent" to="/admin/users"><LuUsersRound />Users</Link> 
+                               
             </div>
-            <div className="w-[calc(100%-300px)] h-full border-8 border-accent rounded-[20px] bg-primary">
+            <div className="w-[calc(100%-300px)] h-full border-8 border-accent rounded-[20px] bg-primary p-4">
                 <Routes>
                     <Route path="/" element={<h1>Orders Page</h1>}/>
-                    <Route path="/products" element={<h1>Products Page</h1>}/>
+                    <Route path="/products" element={<AdminProductsPage />}/>
                     <Route path="/users" element={<h1>Users Page</h1>}/>
+                    <Route path="/add-product" element={<AdminAddProductPage />}/>
                 </Routes>                
             </div>
         </div>
